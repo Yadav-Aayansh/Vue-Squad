2e4806d450da754a57141b1ab803013f8a7ca5a1
Mayank Tripathi <113855408+mayanktripathii@users.noreply.github.com>
2025-01-04 18:09:25 +0530

    Create quiz.py

---
+++ server/app/models/quiz.py
@@ -0,0 +1,5 @@
+class Quiz(db.Model):
+    __tablename__ = 'quizzes'
+    quiz_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    lesson_id = db.Column(db.Integer, db.ForeignKey('lessons.lesson_id'), nullable=False)
